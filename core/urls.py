07ec3218fa24f5f47483bbd7c47fa1470072ca50
Iv4nGomez <xigomjim596@ieshnosmachado.org>
2026-01-30 14:08:17 +0100

    Create route,view,templates to display users

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import home, user_profile
 
 urlpatterns = [
-    
+    path('', home, name='hello'),
+    path('<username>/', user_profile, name='user_profile'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
-# Create your views here.
+def home(request):
+    return HttpResponse('Home')
+
+def user_profile(request, username):
+    user = User.objects.get(username=username)
+    return render(request,'core/user_profile.html', {'user':user})
