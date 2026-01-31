b3f393eae33f7d98b47d0849af22f71cf6b2ca14
Iv4nTech <ivangomezjimenez16@gmail.com>
2026-01-31 12:55:35 +0100

    gitignore update

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, user_profile
+from .views import home, user_profile, list_users
 
 urlpatterns = [
     path('', home, name='hello'),
     path('<username>/', user_profile, name='user_profile'),
+    path('users/', list_users, name='list_users')
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,7 @@ def home(request):
 def user_profile(request, username):
     user = User.objects.get(username=username)
     return render(request,'core/user_profile.html', {'user_profile':user})
+
+def list_users(request):
+    users = User.objects.all()
+    return render(request, 'core/list_users.html', {'users':users})
